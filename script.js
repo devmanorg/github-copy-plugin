6b7fc14378f1c00b4337f3ca4d11a9dbd3e87dd3
Evgeny Evseev <pelid80@gmail.com>
2020-06-12 14:18:52 +0300

    split logic for different hosts

--- script.js
+++ script.js
@@ -93,10 +93,81 @@ async function copyMarkdownSnippetFromOtherSite(){
   await navigator.clipboard.writeText(markdownSnippet);
 }
 
-document.addEventListener('keyup', handleKeyUp, false);
+function wrapWithErrorsHandler(callback){
+  async function wrapper(...args) {
+    try {
+      await callback(...args);
+      console.log('copied');
+    } catch (error) {
+      console.error(error);
+      if (error instanceof DOMException){
+        alert(error.message);
+      } else {
+        alert('Copy plugin is broken');
+      }
+    }
+  }
+  return wrapper;
+}
 
-window.addEventListener("hashchange",function(event){
-  // clear selection when lines of code selected and anchor change
-  // otherwise on GitHub plugin will copy old selection and not lines
-  document.getSelection().removeAllRanges();
-});
+function initGitHub(){
+  async function handleKeyUp(event) {
+    if (event.altKey && event.keyCode == 67){ // Latin or Cyrillic key C
+      event.preventDefault();
+      event.stopPropagation();
+
+      await wrapWithErrorsHandler(copyMarkdownSnippetFromGithub)(event.shiftKey);
+    }
+  }
+
+  document.addEventListener('keyup', handleKeyUp, {
+    capture: false,
+  });
+
+  window.addEventListener("hashchange",function(event){
+    // clear selection when lines of code selected and anchor change
+    // otherwise on GitHub plugin will copy old selection and not lines
+    document.getSelection().removeAllRanges();
+  });
+}
+
+function initReplIt(){
+  async function handleKeyUp(event) {
+    if (event.altKey && event.keyCode == 67){ // Latin or Cyrillic key C
+      event.preventDefault();
+      event.stopPropagation();
+
+      await wrapWithErrorsHandler(copyMarkdownSnippetFromOtherSite)();
+    }
+  }
+
+  document.addEventListener('keyup', handleKeyUp, {
+    capture: false,
+  });
+}
+
+function initNonameSite(){
+  async function handleKeyUp(event) {
+    if (event.altKey && event.keyCode == 67){ // Latin or Cyrillic key C
+      event.preventDefault();
+      event.stopPropagation();
+
+      await wrapWithErrorsHandler(copyMarkdownSnippetFromOtherSite)();
+    }
+  }
+
+  document.addEventListener('keyup', handleKeyUp, {
+    capture: false,
+  });
+}
+
+if (window.location.host == 'github.com'){
+  console.log('CopyPlugin: GitHub detected');
+  initGitHub();
+} else if (window.location.host == 'repl.it'){
+  console.log('CopyPlugin: Repl.it detected');
+  initReplIt();
+} else {
+  console.log('CopyPlugin: Unknown site, so activate default behaviour');
+  initNonameSite();
+}
