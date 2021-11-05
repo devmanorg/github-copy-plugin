903d6c883ca8e35827f04458317dcc4307554210
Evgeny Evseev <pelid80@gmail.com>
2021-11-05 21:16:02 +0300

    fix code formatting

--- enhancements-search/open-dialog.js
+++ enhancements-search/open-dialog.js
@@ -1,96 +1,96 @@
-let modal_dialog = `<style>
-.hide{
-    display: none;
-}
-.modal{
-    /*display: none;*/
-    z-index: 2147483647;  /* max possible value */
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0,0,0,0.3);
-    top:0;
-    left: 0;
-}
-.modal-body{
-    min-height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 30px 10px;
-}
+let modal_dialog = `
+<style>
+  .hide{
+      display: none;
+  }
+  .modal{
+      /*display: none;*/
+      z-index: 2147483647;  /* max possible value */
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0,0,0,0.3);
+      top:0;
+      left: 0;
+  }
+  .modal-body{
+      min-height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 30px 10px;
+  }
 
-.modal-content{
-    background-color: white;
-    color: #000;
-    max-width: 800px;
-    padding: 20px;
-}
-.modal-header{
-    display: flex;
-    justify-content: space-between;
-    font-size: 20px;
-    max-height: 50px;
-    margin: 5px;
-    border-bottom: 1px #eee;
-}
-.modal-close{
-    background-color: transparent;
-    border: 0;
-    font-size: 20px;
-}
-.modal-header h5{
-    margin: 0;
-}
-.modal-main{
-    width: 100%;
-    font-size: 10px;
-    margin: 10px 0;
-}
-.modal-input{
-    margin-bottom: 10px;
-}
-.modal-main h6{
-    margin: 5px;
-    color: #495057;
+  .modal-content{
+      background-color: white;
+      color: #000;
+      max-width: 800px;
+      padding: 20px;
+  }
+  .modal-header{
+      display: flex;
+      justify-content: space-between;
+      font-size: 20px;
+      max-height: 50px;
+      margin: 5px;
+      border-bottom: 1px #eee;
+  }
+  .modal-close{
+      background-color: transparent;
+      border: 0;
+      font-size: 20px;
+  }
+  .modal-header h5{
+      margin: 0;
+  }
+  .modal-main{
+      width: 100%;
+      font-size: 10px;
+      margin: 10px 0;
+  }
+  .modal-input{
+      margin-bottom: 10px;
+  }
+  .modal-main h6{
+      margin: 5px;
+      color: #495057;
 
-}
-.modal-main input{
-    width: 100%;
-}
-.modal-main button{
-    width: 100%;
-    max-height: 50px;
-    font-size: 20px;
-    text-align: left;
-    position: relative;
-    display: block;
-    padding: .75rem 1.25rem;
-    background-color: #fff;
-    border: 1px solid rgba(0,0,0,.125);
-}
+  }
+  .modal-main input{
+      width: 100%;
+  }
+  .modal-main button{
+      width: 100%;
+      max-height: 50px;
+      font-size: 20px;
+      text-align: left;
+      position: relative;
+      display: block;
+      padding: .75rem 1.25rem;
+      background-color: #fff;
+      border: 1px solid rgba(0,0,0,.125);
+  }
 </style>
-  <div class="modal-body">
-
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Типичные улучшения</h5>
-        <button type="button" class="close modal-close js-close-modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-
-      <div class="modal-main">
-        <div class="modal-input" style="min-width: 400px;">
-            <input type="text" id="common-error-search" tabindex="1" class="form-control" autocomplete="off"/>
-              <kbd class="bg-transparent text-muted">Enter</kbd>, чтобы выбрать улучшение. Для поиска по тегу начните название с собаки <code>@</code>.
-        </div>
-        <div id="tags-available" style="min-width: 150px;">
-        </div>
-      </div>
+<div class="modal-body">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title">Типичные улучшения</h5>
+      <button type="button" class="close modal-close js-close-modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
     </div>
 
-</div>`;
+    <div class="modal-main">
+      <div class="modal-input" style="min-width: 400px;">
+          <input type="text" id="common-error-search" tabindex="1" class="form-control" autocomplete="off"/>
+            <kbd class="bg-transparent text-muted">Enter</kbd>, чтобы выбрать улучшение. Для поиска по тегу начните название с собаки <code>@</code>.
+      </div>
+      <div id="tags-available" style="min-width: 150px;">
+      </div>
+    </div>
+  </div>
+</div>
+`;
 
 function createEnhBlock(title, url){
   let enhBlock = document.createElement("div");
